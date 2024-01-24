453390503c54ce935e38f16fe8731039844b4123
JamesAmigo <jijia66@gmail.com>
2024-01-24 08:17:05 -0500

    Encapsulation and interface implementation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,21 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(2, 3);
+        Square square = new Square(5);
+        Circle circle = new Circle(1);
 
         Renderer renderer = new Renderer(rectangle);
-
         renderer.draw();
+
+        Renderer renderer2 = new Renderer(square);
+        renderer2.draw();
+
+        Renderer renderer3 = new Renderer(circle);
+        renderer3.draw();
+
     }
 }
